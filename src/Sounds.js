7e9fd6f829aefc2de7dc9a02361fa6b930874055
Polar stoat <polarstoat@users.noreply.github.com>
2020-05-14 23:05:51 +0100

    Randomly select sounds

--- src/Sounds.js
+++ src/Sounds.js
@@ -5,13 +5,33 @@ import randomInt from 'random-int';
 
 import Sound from './Sound.js';
 
+/**
+ * Total number of sound files available to draw from
+ * @todo Generate this integer dynamically based on content of /public/audio/ folder
+ * @type {Number}
+ */
+const NUMBER_OF_SOUNDS = 10;
+/**
+ * How many different sound sliders to display at once
+ * @type {Number}
+ */
+const SOUNDS_TO_DISPLAY = 6;
+
+const soundIds = [];
+while (soundIds.length < SOUNDS_TO_DISPLAY) {
+  const ran = randomInt(NUMBER_OF_SOUNDS);
+  if (soundIds.indexOf(ran) === -1) soundIds.push(ran);
+}
+
 class Sounds extends Component {
   render() {
+    const sounds = soundIds.map(id => (
+      <Sound id={id} backgroundHue={randomInt(360)} />
+    ));
+
     return (
       <Form>
-        <Sound id={1} backgroundHue={randomInt(360)} />
-        <Sound id={2} backgroundHue={randomInt(360)} />
-        <Sound id={3} backgroundHue={randomInt(360)} />
+        {sounds}
       </Form>
     );
   }
