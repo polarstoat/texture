c74cbeabe37deb614e7ca4e21bbfb1ae1ed4c8b2
Polar stoat <polarstoat@users.noreply.github.com>
2020-05-14 15:03:53 +0100

    Add state to Slider component

--- src/Slider.js
+++ src/Slider.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react';
 import FormControl from 'react-bootstrap/FormControl';
 
 class Slider extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.onChange(event.target.valueAsNumber);
+  }
+
   render() {
     return (
-      <FormControl type="range" min={0} max={1} step={0.01} custom />
+      <FormControl value={this.props.value} onChange={this.handleChange} type="range" min={0} max={1} step={0.01} custom />
     );
   }
 }
--- src/Sound.js
+++ src/Sound.js
@@ -7,10 +7,24 @@ import Slider from './Slider.js';
 import './Sound.css';
 
 class Sound extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      volume: 0,
+    };
+
+    this.handleVolumeChange = this.handleVolumeChange.bind(this);
+  }
+
+  handleVolumeChange(volume) {
+    this.setState({ volume });
+  }
+
   render() {
     return (
       <Form.Group className="sound" style={{ backgroundColor: `hsl(${this.props.backgroundHue}, 58%, 75%)` }}>
-        <Slider />
+        <Slider value={this.state.volume} onChange={this.handleVolumeChange} />
       </Form.Group>
     );
   }
