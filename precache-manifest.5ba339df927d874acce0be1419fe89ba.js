fd49a6ca20c6472d50f6ca3542e8bc8ec203f2c2
Polar stoat <polarstoat@users.noreply.github.com>
2020-05-18 17:35:57 +0100

    Updates

---
+++ precache-manifest.5ba339df927d874acce0be1419fe89ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e2c3c93c499316e9a86591bb265ee66",
+    "url": "/texture/index.html"
+  },
+  {
+    "revision": "19d44dcaa4f984dd3cfa",
+    "url": "/texture/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4d7b8bad0d2615f72227",
+    "url": "/texture/static/css/main.dca33029.chunk.css"
+  },
+  {
+    "revision": "19d44dcaa4f984dd3cfa",
+    "url": "/texture/static/js/2.a8ff92ed.chunk.js"
+  },
+  {
+    "revision": "30fe6dcd7a0fad1aafcf1daae47fb9e0",
+    "url": "/texture/static/js/2.a8ff92ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d7b8bad0d2615f72227",
+    "url": "/texture/static/js/main.4d681621.chunk.js"
+  },
+  {
+    "revision": "bc7fd2b91a059cf663e3",
+    "url": "/texture/static/js/runtime-main.e6b49b19.js"
+  }
+]);
