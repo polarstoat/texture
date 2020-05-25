73c8600852baad5ceeeea22dec08d3258c22cd70
Polar stoat <polarstoat@users.noreply.github.com>
2020-05-25 12:38:03 +0100

    Add Randomise button

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
 class Header extends Component {
   render() {
     return (
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand>Texture</Navbar.Brand>
+        <Button variant="outline-light" className="ml-auto">Randomise</Button>
       </Navbar>
     );
   }
