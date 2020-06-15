7a7b1b2828dae223773de678dcfbb976944a127a
Polar stoat <polarstoat@users.noreply.github.com>
2020-06-15 16:00:48 +0100

    Updates

--- precache-manifest.6f9bc9ecd81db144899bce80e1e8bb50.js
+++ precache-manifest.6f9bc9ecd81db144899bce80e1e8bb50.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f9a5fc04351ae78f092d880a9cdec61f",
+    "revision": "ea92a9eab0211f2fb593b616498c3168",
     "url": "/index.html"
   },
   {
-    "revision": "b5658966d40b2dfbdf14",
+    "revision": "ec8219a4239aa9a9c6d3",
     "url": "/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "fc83bac766bad0fcade4",
+    "revision": "a6ed7b406832a6275644",
     "url": "/static/css/main.7677a3c4.chunk.css"
   },
   {
-    "revision": "b5658966d40b2dfbdf14",
-    "url": "/static/js/2.a59c794d.chunk.js"
+    "revision": "ec8219a4239aa9a9c6d3",
+    "url": "/static/js/2.2aafd19b.chunk.js"
   },
   {
     "revision": "bf4163e661ae8474b835f737646cfc8f",
-    "url": "/static/js/2.a59c794d.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.2aafd19b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fc83bac766bad0fcade4",
-    "url": "/static/js/main.85381d13.chunk.js"
+    "revision": "a6ed7b406832a6275644",
+    "url": "/static/js/main.556e93ca.chunk.js"
   },
   {
     "revision": "e9ca3fab24f28fb0a68b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.43a01f172941024118de911fb8015c38.js"
+  "/precache-manifest.6f9bc9ecd81db144899bce80e1e8bb50.js"
 );
 
 self.addEventListener('message', (event) => {
