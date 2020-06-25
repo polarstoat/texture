14a69a7f5e00b5630d7629fee9d76c0b2a2c2e4e
Polar stoat <polarstoat@users.noreply.github.com>
2020-06-25 20:11:29 +0100

    fix body typo

--- src/BodyText.js
+++ src/BodyText.js
@@ -7,7 +7,7 @@ class BodyText extends Component {
     return (
       <Container fluid className="pt-4 px-4">
         <p>Each time you refresh, Texture randomly chooses 6 audio loops and provides volume sliders for mixing. Once you have created a mix you like, you can copy and share it’s unique URL.</p>
-        <p>While there is no proscribed use, Texture was originally proposed to provide texture and atmosphere for synth music and sonic inspiration for soundtracking. With an ever growing pool of sounds, Texture can breathe life into an overly clean mix in all kinds of unexpected ways. It is both intense, absorbing and capable of a huge range of presences, characteristics and readings.</p>
+        <p>While there is no prescribed use, Texture was originally proposed to provide texture and atmosphere for synth music and sonic inspiration for soundtracking. With an ever growing pool of sounds, Texture can breathe life into an overly clean mix in all kinds of unexpected ways. It is both intense, absorbing and capable of a huge range of presences, characteristics and readings.</p>
         <p>Whenever the sounds you’re working with need a bit of roughing up, or needs a refresh or a different perspective, give Texture a try.</p>
         <p>You could also try adding texture to:</p>
         <ul>
