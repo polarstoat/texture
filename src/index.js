453e0f8d47c7163267f595be337c0b6397cc5285
Polar stoat <polarstoat@users.noreply.github.com>
2020-06-29 13:08:59 +0100

    Customise Bootstrap SCSS

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.custom.scss';
 import App from './App';
 
 ReactDOM.render(
