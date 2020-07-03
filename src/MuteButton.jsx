5b86c7ca95d2bc64f007821cb654444eb48a22bb
Polar stoat <polarstoat@users.noreply.github.com>
2020-07-03 22:30:30 +0100

    Swap mute/unmute icons
    Icon now display current state

--- src/MuteButton.jsx
+++ src/MuteButton.jsx
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import { BsVolumeMuteFill, BsVolumeUpFill } from 'react-icons/bs';
 
+const iconSize = '1.5em';
+
 class MuteButton extends PureComponent {
   render() {
     const { muted, onToggle } = this.props;
 
     return (
-      <Button variant={muted ? 'light' : 'secondary'} className={muted ? 'muted' : 'unmuted'} onClick={onToggle}>{muted ? <BsVolumeUpFill size="1.5em" /> : <BsVolumeMuteFill size="1.5em" />}</Button>
+      <Button variant={muted ? 'light' : 'secondary'} className={muted ? 'muted' : 'unmuted'} onClick={onToggle}>{muted ? <BsVolumeMuteFill size={iconSize} /> : <BsVolumeUpFill size={iconSize} />}</Button>
     );
   }
 }
