342b1f1b9386ae88ea1992e2479f95bbf4f359f3
Polar stoat <polarstoat@users.noreply.github.com>
2020-07-05 15:18:53 +0100

    Fix bottom margin of donate button

--- src/BodyText.jsx
+++ src/BodyText.jsx
@@ -13,7 +13,7 @@ class BodyText extends PureComponent {
           <Col md={6}>
             <p>Each time you shuffle, Texture randomly chooses 6 audio loops and colours, and provides volume sliders for mixing. Once you have created a Texture you like, you can copy and share it’s unique URL, which will save the audio loops, the colours, and the volume levels for future use.</p>
             <p>While there is no prescribed use, Texture was originally proposed to provide texture and atmosphere for synth music and sonic inspiration for soundtracking. Its colourful and simple interface encourages playfulness over perfection. With an ever growing pool of sounds, Texture can breathe life into an overly clean mix in all kinds of unexpected ways. It is both intense, absorbing and capable of a huge range of presences, characteristics and readings.</p>
-            <a className="btn btn-donate" role="button" href="https://paypal.me/graemejwalker" target="_blank" rel="noreferrer noopener">Donate via PayPal</a>
+            <p><a className="btn btn-donate" role="button" href="https://paypal.me/graemejwalker" target="_blank" rel="noreferrer noopener">Donate via PayPal</a></p>
           </Col>
           <Col md={6}>
             <p>Whenever the sounds you’re working with need a bit of roughing up, or needs a refresh or a different perspective, give Texture a try.</p>
