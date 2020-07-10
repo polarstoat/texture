99296a4b8ae8cbc1af23054b216f2507e1abc0f9
Polar stoat <polarstoat@users.noreply.github.com>
2020-07-10 12:48:05 +0100

    Add cookie notice

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,6 +6,7 @@ class Footer extends PureComponent {
     return (
       <Navbar>
         <Navbar.Text><small>Texture &copy; Spike Padley and Graeme Walker 2020</small></Navbar.Text>
+        <Navbar.Text className="ml-auto"><small>Texture uses cookies to save your current Texture in between sessions and measure traffic</small></Navbar.Text>
       </Navbar>
     );
   }
