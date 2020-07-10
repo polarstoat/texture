5d91818ffb74f61222c30001ed9522f5cea7a6c5
Polar stoat <polarstoat@users.noreply.github.com>
2020-07-10 12:18:12 +0100

    Create handleClick method

--- src/ShareURL.jsx
+++ src/ShareURL.jsx
@@ -7,6 +7,11 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import './ShareURL.css';
 
 class ShareURL extends PureComponent {
+  handleClick = (event) => {
+    event.target.focus();
+    event.target.select();
+  }
+
   render() {
     const { code } = this.props;
 
@@ -16,7 +21,7 @@ class ShareURL extends PureComponent {
           <InputGroup.Prepend>
             <InputGroup.Text id="shareable-url-label">Shareable URL</InputGroup.Text>
           </InputGroup.Prepend>
-          <Form.Control type="text" id="shareable-url" aria-label="Shareable URL for this Texture" aria-describedby="shareable-url-label" readOnly value={`${window.location.origin}${window.location.pathname}#${code}`} onClick={(event) => { event.target.focus(); event.target.select(); }} />
+          <Form.Control type="text" id="shareable-url" aria-label="Shareable URL for this Texture" aria-describedby="shareable-url-label" readOnly value={`${window.location.origin}${window.location.pathname}#${code}`} onClick={this.handleClick} />
         </InputGroup>
       </Container>
     );
