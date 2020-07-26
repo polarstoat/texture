55331ab9a8321b732de07fe4c12ee73d3f3990c5
Polar stoat <polarstoat@users.noreply.github.com>
2020-07-26 12:19:08 +0100

    Add link to learn more about cookies

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,14 @@
 import React, { PureComponent } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 
+/* eslint-disable react/jsx-one-expression-per-line */
+
 class Footer extends PureComponent {
   render() {
     return (
       <Navbar>
         <Navbar.Text><small>Texture &copy; Spike Padley and Graeme Walker 2020</small></Navbar.Text>
-        <Navbar.Text className="ml-auto"><small>Texture uses cookies to save your current Texture in between sessions and measure traffic</small></Navbar.Text>
+        <Navbar.Text className="ml-auto"><small>Texture uses cookies to save your current Texture in between sessions and measure traffic. <a href="https://www.cookiesandyou.com/" target="_blank" rel="noreferrer noopener">Learn more about cookies</a>.</small></Navbar.Text>
       </Navbar>
     );
   }
